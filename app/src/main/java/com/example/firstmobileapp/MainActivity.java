3ac14c78f934dea13dcbcf000cd1ad7ec66f3bd1
folakemi10 <f.shofu@wustl.edu>
2022-01-13 22:56:48 -0600

    done

---
+++ app/src/main/java/com/example/firstmobileapp/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.firstmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = ((TextView) findViewById(R.id.text));
+
+        //user can tap on a button to change text color
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //change the text color of the label
+               textView.setTextColor( getResources().getColor(R.color.purple_200));
+            }
+        });
+
+        //user can tap on button to change background color
+        findViewById(R.id.chngBackgroundButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.parent).setBackgroundColor( getResources().getColor(R.color.white));
+            }
+        });
+
+        //user can change text
+        findViewById(R.id.chngTextButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Code Path is Awesome");
+            }
+        });
+
+        //user can tap on background to reset all views to default
+        findViewById(R.id.parent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //reset text
+                textView.setText("Hello from Folakemi!");
+                //reset color
+                textView.setTextColor(getResources().getColor(R.color.black));
+                //reset background
+                findViewById(R.id.parent).setBackgroundColor(getResources().getColor(R.color.blue_green));
+            }
+        });
+
+    }
+}
